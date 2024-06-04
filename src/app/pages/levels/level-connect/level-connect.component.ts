dc7f57f8db7bd1ea6e1308b890c19c77e2d56d80
pkuske <pkuske@live.de>
2024-06-04 20:51:11 +0200

    changed connect lvl

--- src/app/pages/levels/level-connect/level-connect.component.ts
+++ src/app/pages/levels/level-connect/level-connect.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { connectData } from './connectData';
 import { wordDataModel } from 'src/app/models/wordDataModel';
+
+interface WordPair {
+  dialect: string;
+  dutch: string;
+  selected: boolean;
+  matched: boolean;
+}
 
 @Component({
   selector: 'app-level-connect',
@@ -8,30 +15,44 @@ import { wordDataModel } from 'src/app/models/wordDataModel';
   styleUrls: ['./level-connect.component.scss']
 })
 export class LevelConnectComponent { 
-  buttonDisabledStates: boolean[] = [false, false, false, false, false, false, false, false];
-  buttonMatchStates: boolean[] = [false, false, false, false, false, false, false, false];
-  clickedButton: number[] = [];
+  wordPairs: WordPair[] = [
+    { dialect: 'verchet', dutch: 'vork', selected: false, matched: false },
+    { dialect: 'mets', dutch: 'mes', selected: false, matched: false },
+    { dialect: 'glaas', dutch: 'glas', selected: false, matched: false },
+    { dialect: 'tleur', dutch: 'bord', selected: false, matched: false },
+    // Add more words as needed
+  ];
 
-  wordSet = connectData.translations;
+  selectedEnglishIndex: number | null = null;
+  selectedDutchIndex: number | null = null;
 
-  onButtonClick(index: number) {
-    console.log(this.wordSet);
-    console.log(this.clickedButton);
-    this.buttonDisabledStates[index] = true;
+  constructor() { }
 
-    if(this.clickedButton.length == 0) {
-      this.clickedButton[0] = index;
-    } else if(this.clickedButton.length == 2) {
-      this.clickedButton = [];
-    } 
-    else {
-      this.clickedButton[1] = index;
+  
+  // buttonDisabledStates: boolean[] = [false, false, false, false, false, false, false, false];
+  // buttonMatchStates: boolean[] = [false, false, false, false, false, false, false, false];
+  // clickedButton: number[] = [];
+
+  // wordSet = connectData.translations;
+
+  // onButtonClick(index: number) {
+  //   console.log(this.wordSet);
+  //   console.log(this.clickedButton);
+  //   this.buttonDisabledStates[index] = true;
+
+  //   if(this.clickedButton.length == 0) {
+  //     this.clickedButton[0] = index;
+  //   } else if(this.clickedButton.length == 2) {
+  //     this.clickedButton = [];
+  //   } 
+  //   else {
+  //     this.clickedButton[1] = index;
     
-      if(this.clickedButton[0] % 2 === 0 && this.clickedButton[1] % 2 === 0 || 
-        this.clickedButton[0] % 2 !== 0 && this.clickedButton[1] % 2 !== 0) {
-          this.buttonDisabledStates[this.clickedButton[0]] = false;
-          this.buttonDisabledStates[this.clickedButton[1]] = false;
-      }
-    }
-  }
+  //     if(this.clickedButton[0] % 2 === 0 && this.clickedButton[1] % 2 === 0 || 
+  //       this.clickedButton[0] % 2 !== 0 && this.clickedButton[1] % 2 !== 0) {
+  //         this.buttonDisabledStates[this.clickedButton[0]] = false;
+  //         this.buttonDisabledStates[this.clickedButton[1]] = false;
+  //     }
+  //   }
+  // }
 }
